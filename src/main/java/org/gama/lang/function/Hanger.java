5c9517429a7b8ef4d4b00bda9425e4c6eafe515d
Guillaume Mary <guiom.mary@gmail.com>
2018-01-28 23:25:18 +0100

    Simple Holder created from a Hanger and a Supplier

--- src/main/java/org/gama/lang/function/Hanger.java
+++ src/main/java/org/gama/lang/function/Hanger.java
@@ -1,7 +1,9 @@
 package org.gama.lang.function;
 
+import java.util.function.Supplier;
+
 /**
- * A simple interface to define that you can hang something on it 
+ * A simple interface to define that you can hang something on it, opposit to {@link Supplier}
  * 
  * @author Guillaume Mary
  */
@@ -9,4 +11,31 @@ package org.gama.lang.function;
 public interface Hanger<T> {
 	
 	void set(T value);
+	
+	/**
+	 * A {@link Hanger} and {@link Supplier}
+	 * 
+	 * @param <T>
+	 */
+	class Holder<T> implements Hanger<T>, Supplier<T> {
+		
+		private T payload;
+		
+		public Holder() {
+		}
+		
+		public Holder(T payload) {
+			this.payload = payload;
+		}
+		
+		@Override
+		public void set(T value) {
+			this.payload = value;
+		}
+		
+		@Override
+		public T get() {
+			return payload;
+		}
+	}
 }
