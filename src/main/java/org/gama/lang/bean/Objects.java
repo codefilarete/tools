f95ed23eba3300d1ab33c5af3d3ee967b57abfa8
Guillaume Mary <guiom.mary@gmail.com>
2020-03-21 20:25:53 +0100

    Adding Objects.fallback method

--- src/main/java/org/gama/lang/bean/Objects.java
+++ src/main/java/org/gama/lang/bean/Objects.java
@@ -18,6 +18,23 @@ public class Objects {
 	}
 	
 	/**
+	 * Returns a fallback value if the given input equals a matching one
+	 *
+	 * @param input value that may equals the matching one
+	 * @param matchingCase value on which fallback value will be return
+	 * @param fallbackValue value returned when input equals matching value
+	 * @param <C> type of mangaed value
+	 * @return fallbackValue if input equals matching case
+	 */
+	public static <C> C fallback(C input, C matchingCase, C fallbackValue) {
+		if (java.util.Objects.equals(input, matchingCase)) {
+			return fallbackValue;
+		} else {
+			return input;
+		}
+	}
+	
+	/**
 	 * Static method to negate the given predicate so one can write {@code not(String::contains)}.
 	 * 
 	 * @param predicate any {@link Predicate}, a method reference is prefered else this method as no purpose and can be replaced by {@link Predicate#negate}
--- src/test/java/org/gama/lang/bean/ObjectsTest.java
+++ src/test/java/org/gama/lang/bean/ObjectsTest.java
@@ -3,6 +3,7 @@ package org.gama.lang.bean;
 import org.gama.lang.collection.Arrays;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
@@ -13,5 +14,14 @@ class ObjectsTest {
 	@Test
 	void not() {
 		assertFalse(Arrays.asList("a", "a", "a").stream().allMatch(Objects.not("a"::equals)));
+	}
+	
+	@Test
+	void decode() {
+		assertEquals("B", Objects.fallback("A", "A", "B"));
+		assertEquals("C", Objects.fallback("C", "A", "B"));
+		assertEquals("B", Objects.fallback(null, null, "B"));
+		assertEquals("A", Objects.fallback("A", null, "B"));
+		assertEquals(null, Objects.fallback(null, "A", "B"));
 	}
 }
