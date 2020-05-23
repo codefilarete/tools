469dbf72abeb5dee6efe7603568978052af125ea
Guillaume Mary <guiom.mary@gmail.com>
2020-05-23 09:27:22 +0200

    Adding methods to ThreadLocals. Avoid setting value when one is already present in ThreadLocal.

--- src/main/java/org/gama/lang/ThreadLocals.java
+++ src/main/java/org/gama/lang/ThreadLocals.java
@@ -1,8 +1,12 @@
 package org.gama.lang;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 import org.gama.lang.function.Hanger;
+import org.gama.lang.function.ThrowingConsumer;
+import org.gama.lang.function.ThrowingFunction;
 import org.gama.lang.function.ThrowingRunnable;
 import org.gama.lang.function.ThrowingSupplier;
 
@@ -37,7 +41,36 @@ public class ThreadLocals {
 	 */
 	public static <T, E extends Throwable> void doWithThreadLocal(ThreadLocal<T> threadLocal, Supplier<T> factory, ThrowingRunnable<E> runnable) 
 			throws E {
-		doWithThreadLocal(threadLocal, factory, (ThrowingSupplier<Void, E>) () -> { runnable.run(); return null; });
+		doWithThreadLocal(threadLocal, factory, (ThrowingFunction<T, Void, E>) t -> { runnable.run(); return null; });
+	}
+	
+	/**
+	 * Runs some code with a {@link ThreadLocal} and cleans it afterwards by removing the instance of the {@link ThreadLocal}
+	 * through {@link ThreadLocal#remove()}.
+	 * 
+	 * @param threadLocal the {@link ThreadLocal} to be used
+	 * @param factory the supplier of the instance assigned to {@link ThreadLocal}
+	 * @param runnable some code to be run
+	 * @param <T> type of bean assigned to the ThreadLocal
+	 * @see AutoRemoveThreadLocal
+	 */
+	public static <T> void doWithThreadLocal(ThreadLocal<T> threadLocal, Supplier<T> factory, Consumer<T> runnable) {
+		doWithThreadLocal(threadLocal, factory, (ThrowingConsumer<T, RuntimeException>) runnable::accept);
+	}
+	
+	/**
+	 * Runs some code with a {@link ThreadLocal} and cleans it afterwards by removing the instance of the {@link ThreadLocal}
+	 * through {@link ThreadLocal#remove()}.
+	 *
+	 * @param threadLocal the {@link ThreadLocal} to be used
+	 * @param factory the supplier of the instance assigned to {@link ThreadLocal}
+	 * @param runnable some code to be run
+	 * @param <T> type of bean assigned to the ThreadLocal
+	 * @see AutoRemoveThreadLocal
+	 */
+	public static <T, E extends Throwable> void doWithThreadLocal(ThreadLocal<T> threadLocal, Supplier<T> factory, ThrowingConsumer<T, E> runnable) 
+			throws E {
+		doWithThreadLocal(threadLocal, factory, (ThrowingFunction<T, Void, E>) t -> { runnable.accept(t); return null; });
 	}
 	
 	/**
@@ -66,10 +99,43 @@ public class ThreadLocals {
 	 */
 	public static <T, O, E extends Throwable> O doWithThreadLocal(ThreadLocal<T> threadLocal, Supplier<T> factory, ThrowingSupplier<O, E> runnable) 
 			throws E {
-		threadLocal.set(factory.get());
+		return doWithThreadLocal(threadLocal, factory, (ThrowingFunction<T, O, E>) t -> runnable.get());
+	}
+	
+	/**
+	 * Runs some code with a {@link ThreadLocal} and cleans it afterwards by removing the instance of the {@link ThreadLocal}
+	 * through {@link ThreadLocal#remove()}.
+	 *
+	 * @param threadLocal the {@link ThreadLocal} to be used
+	 * @param factory the supplier of the instance assigned to {@link ThreadLocal}
+	 * @param runnable some code to be run
+	 * @param <T> type of bean assigned to the ThreadLocal
+	 * @see AutoRemoveThreadLocal
+	 */
+	public static <T, O> O doWithThreadLocal(ThreadLocal<T> threadLocal, Supplier<T> factory, Function<T, O> runnable) {
+		return doWithThreadLocal(threadLocal, factory, (ThrowingFunction<T, O, RuntimeException>) runnable::apply);
+	}
+	
+	/**
+	 * Runs some code with a {@link ThreadLocal} and cleans it afterwards by removing the instance of the {@link ThreadLocal}
+	 * through {@link ThreadLocal#remove()}.
+	 *
+	 * @param threadLocal the {@link ThreadLocal} to be used
+	 * @param factory the supplier of the instance assigned to {@link ThreadLocal}
+	 * @param runnable some code to be run
+	 * @param <T> type of bean assigned to the ThreadLocal
+	 * @see AutoRemoveThreadLocal
+	 */
+	public static <T, O, E extends Throwable> O doWithThreadLocal(ThreadLocal<T> threadLocal, Supplier<T> factory, ThrowingFunction<T, O, E> runnable)
+			throws E {
+		T value = threadLocal.get();
+		if (value == null) {
+			value = factory.get();
+			threadLocal.set(value);
+		}
 		
 		try (AutoRemoveThreadLocal<T> ignored = new AutoRemoveThreadLocal<>(threadLocal)) {
-			return runnable.get();
+			return runnable.apply(value);
 		}
 	}
 	
--- src/test/java/org/gama/lang/ThreadLocalsTest.java
+++ src/test/java/org/gama/lang/ThreadLocalsTest.java
@@ -1,6 +1,6 @@
 package org.gama.lang;
 
-import java.util.function.Supplier;
+import java.util.function.Function;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,10 +13,19 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 public class ThreadLocalsTest {
 	
 	@Test
-	public void testFoWithThreadLocal() {
+	void withThreadLocal_function_noValuePresent() {
 		ThreadLocal<String> targetThreadLocal = new ThreadLocal<>();
-		String result = ThreadLocals.doWithThreadLocal(targetThreadLocal, () -> "Hello ", (Supplier<String>) () -> targetThreadLocal.get() + "world !");
+		String result = ThreadLocals.doWithThreadLocal(targetThreadLocal, () -> "Hello ", (Function<String, String>) s -> s + "world !");
 		assertEquals("Hello world !", result);
+		assertNull(targetThreadLocal.get());
+	}
+	
+	@Test
+	void withThreadLocal_function_valueAlreadyPresent() {
+		ThreadLocal<String> targetThreadLocal = new ThreadLocal<>();
+		targetThreadLocal.set("Hi ");
+		String result = ThreadLocals.doWithThreadLocal(targetThreadLocal, () -> "Hello ", (Function<String, String>) s -> s + "world !");
+		assertEquals("Hi world !", result);
 		assertNull(targetThreadLocal.get());
 	}
 }
