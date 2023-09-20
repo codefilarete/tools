c00c556b7eecbd3a4cb1df7ae15518aeb700a0e3
Guillaume Mary <guiom.mary@gmail.com>
2023-09-20 22:13:19 +0200

    Fixing indent char

--- src/main/java/org/codefilarete/tool/collection/StreamSplitter.java
+++ src/main/java/org/codefilarete/tool/collection/StreamSplitter.java
@@ -7,57 +7,57 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 /**
-* Splits a stream into some others according to given filters.
-*
-* @author Guillaume Mary
-*/
+ * Splits a stream into some others according to given filters.
+ *
+ * @author Guillaume Mary
+ */
 public class StreamSplitter<T> {
-  
-   private final Stream<T> stream;
-  
-   private final Set<Splitter> splitters = new HashSet<>();
-  
-   public StreamSplitter(Stream<T> stream) {
-      this.stream = stream;
-   }
-  
-   /**
-    * Defines a rule to dispatch elements to a {@link Consumer}
-    *
-    * @param acceptanceFilter should returns true for sending element to given {@link Consumer}
-    * @param consumer function called with accepted element
-    * @return this
-    */
-   public StreamSplitter<T> dispatch(Predicate<T> acceptanceFilter, Consumer<T> consumer) {
-      this.splitters.add(new Splitter(acceptanceFilter, consumer));
-      return this;
-   }
-  
-   /**
-    * Triggers dispatching of elements into other {@link Stream} as defines through {@link #dispatch(Predicate, Consumer)}.
-    * This is a terminal operation.
-    */
-   public void split() {
-      stream.forEach(t -> splitters.forEach(splitter -> {
-         if (splitter.splitter.test(t)) {
-            splitter.consumer.accept(t);
-         }
-      }));
-   }
-  
-   /**
-    * Internal storage of condition and consumer
-    */
-   private class Splitter {
-      
-      private final Predicate<T> splitter;
-      private final Consumer<T> consumer;
-     
-      private Splitter(Predicate<T> splitter, Consumer<T> consumer) {
-         this.splitter = splitter;
-         this.consumer = consumer;
-      }
-   }
+	
+	private final Stream<T> stream;
+	
+	private final Set<Splitter> splitters = new HashSet<>();
+	
+	public StreamSplitter(Stream<T> stream) {
+		this.stream = stream;
+	}
+	
+	/**
+	 * Defines a rule to dispatch elements to a {@link Consumer}
+	 *
+	 * @param acceptanceFilter should returns true for sending element to given {@link Consumer}
+	 * @param consumer function called with accepted element
+	 * @return this
+	 */
+	public StreamSplitter<T> dispatch(Predicate<T> acceptanceFilter, Consumer<T> consumer) {
+		this.splitters.add(new Splitter(acceptanceFilter, consumer));
+		return this;
+	}
+	
+	/**
+	 * Triggers dispatching of elements into other {@link Stream} as defines through {@link #dispatch(Predicate, Consumer)}.
+	 * This is a terminal operation.
+	 */
+	public void split() {
+		stream.forEach(t -> splitters.forEach(splitter -> {
+			if (splitter.splitter.test(t)) {
+				splitter.consumer.accept(t);
+			}
+		}));
+	}
+	
+	/**
+	 * Internal storage of condition and consumer
+	 */
+	private class Splitter {
+		
+		private final Predicate<T> splitter;
+		private final Consumer<T> consumer;
+		
+		private Splitter(Predicate<T> splitter, Consumer<T> consumer) {
+			this.splitter = splitter;
+			this.consumer = consumer;
+		}
+	}
 }
 
 
